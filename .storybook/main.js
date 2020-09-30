0e785224a7c46b6c2dd67d19a5823bda9b4bd078
swood <stephen.wood@hoffmann-group.com>
2020-09-30 16:41:50 +0200

    Add docker and storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mjs)',
+  ],
+  'addons': [
+    '@storybook/addon-viewport/register',
+  ],
+};
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = ({ config }) => {
+	config.resolve.modules.push(path.resolve(__dirname, "../src"));
+
+	config.module.rules.push({
+		test: /\.(scss)$/,
+		include: path.resolve(__dirname, "./../"),
+		use: [
+			"style-loader",
+			{
+				loader: "css-loader",
+				options: {
+					sourceMap: true,
+					importLoaders: 1
+				}
+			},
+			{
+				loader: "postcss-loader",
+				options: {
+					autoprefixer: {
+						browsers: ["last 2 versions"]
+					},
+					sourceMap: true,
+					ident: "postcss",
+					plugins: () => [autoprefixer]
+				}
+			},
+			{
+				loader: "sass-loader",
+				options: {
+					sourceMap: true,
+					// https://www.npmjs.com/package/sass-loader
+					implementation: require('node-sass'),
+				}
+			}
+		]
+	});
+
+	return config;
+};
