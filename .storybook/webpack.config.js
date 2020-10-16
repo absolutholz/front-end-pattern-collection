89b6207c048a944b0f5f1db60068ae449a924580
swood <stephen.wood@hoffmann-group.com>
2020-10-16 15:30:17 +0200

    Update setup and mjs loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = ({ config }) => {
-	config.resolve.modules.push(path.resolve(__dirname, "../src"));
+	// config.resolve.modules.push(path.resolve(__dirname, "../src"));
+	config.resolve.modules = [
+		...(config.resolve.modules || []),
+		path.resolve('./src'),
+		// path.resolve('./'),
+	];
 
 	config.module.rules.push({
 		test: /\.(scss)$/,
